026c2eea26ffa13e75c55177c0103d01dc5b5a3c
gisphm <phm8341@gmail.com>
2014-05-21 13:32:55 +0800

    basic struct usage in go

--- learngo.go
+++ learngo.go
@@ -2,37 +2,34 @@ package main
 
 import "fmt"
 
-type testInt func(int) bool
-
-func isOdd(integer int) bool {
-	if integer%2 == 0 {
-		return false
-	}
-	return true
+type person struct {
+	name string
+	age  int
 }
 
-func isEven(integer int) bool {
-	if integer%2 == 0 {
-		return true
+func Older(p1, p2 person) (person, int) {
+	if p1.age > p2.age {
+		return p1, p1.age - p2.age
 	}
-	return false
-}
-
-func filter(slice []int, f testInt) []int {
-	var result []int
-	for _, value := range slice {
-		if f(value) {
-			result = append(result, value)
-		}
-	}
-	return result
+	return p2, p2.age - p1.age
 }
 
 func main() {
-	slice := []int{1, 2, 3, 4, 5, 7}
-	fmt.Println("slice = ", slice)
-	odd := filter(slice, isOdd)
-	fmt.Println("Odd elements of slice are: ", odd)
-	even := filter(slice, isEven)
-	fmt.Println("Even elements of slice are: ", even)
+	var tom person
+	tom.name, tom.age = "Tom", 18
+
+	bob := person{age: 25, name: "Bob"}
+
+	paul := person{"Paul", 43}
+
+	tb_Older, tb_diff := Older(tom, bob)
+	tp_Older, tp_diff := Older(tom, paul)
+	bp_Older, bp_diff := Older(bob, paul)
+
+	fmt.Printf("Of %s and %s, %s is older by %d years\n",
+		tom.name, bob.name, tb_Older.name, tb_diff)
+	fmt.Printf("Of %s and %s, %s is older by %d years\n",
+		tom.name, paul.name, tp_Older.name, tp_diff)
+	fmt.Printf("Of %s and %s, %s is older by %d years\n",
+		bob.name, paul.name, bp_Older.name, bp_diff)
 }
