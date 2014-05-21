6a8ef8320aef4b9e1dfbf6eb94199e3cee1c9aac
gisphm <phm8341@gmail.com>
2014-05-21 13:52:01 +0800

    anynomous struct usage

--- learngo.go
+++ learngo.go
@@ -2,34 +2,32 @@ package main
 
 import "fmt"
 
-type person struct {
-	name string
-	age  int
+type Human struct {
+	name   string
+	age    int
+	weight int
 }
 
-func Older(p1, p2 person) (person, int) {
-	if p1.age > p2.age {
-		return p1, p1.age - p2.age
-	}
-	return p2, p2.age - p1.age
+type Student struct {
+	Human
+	speciality string
 }
 
 func main() {
-	var tom person
-	tom.name, tom.age = "Tom", 18
+	mark := Student{Human{"Mark", 25, 120}, "Computer Science"}
 
-	bob := person{age: 25, name: "Bob"}
+	fmt.Println("His name is ", mark.name)
+	fmt.Println("His age is ", mark.age)
+	fmt.Println("His weight is ", mark.weight)
+	fmt.Println("His speciality is ", mark.speciality)
 
-	paul := person{"Paul", 43}
-
-	tb_Older, tb_diff := Older(tom, bob)
-	tp_Older, tp_diff := Older(tom, paul)
-	bp_Older, bp_diff := Older(bob, paul)
-
-	fmt.Printf("Of %s and %s, %s is older by %d years\n",
-		tom.name, bob.name, tb_Older.name, tb_diff)
-	fmt.Printf("Of %s and %s, %s is older by %d years\n",
-		tom.name, paul.name, tp_Older.name, tp_diff)
-	fmt.Printf("Of %s and %s, %s is older by %d years\n",
-		bob.name, paul.name, bp_Older.name, bp_diff)
+	mark.speciality = "AI"
+	fmt.Println("Mark change his speciality")
+	fmt.Println("His speciality is ", mark.speciality)
+	fmt.Println("Mark become old")
+	mark.age = 46
+	fmt.Println("His age is ", mark.age)
+	fmt.Println("Mark is not an athlet anymore")
+	mark.weight += 60
+	fmt.Println("His weight is ", mark.weight)
 }
