7a040b9293fd0b07245711fbd47413f3b734fa9c
gisphm <phm8341@gmail.com>
2014-05-21 10:35:22 +0800

    multi return value function

--- learngo.go
+++ learngo.go
@@ -2,20 +2,14 @@ package main
 
 import "fmt"
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+func SumAndMultiply(A, B int) (int, int) {
+	return A + B, A * B
 }
 
 func main() {
-	x, y, z := 3, 4, 5
+	x, y := 3, 4
+	xPLUSy, xTIMESy := SumAndMultiply(x, y)
 
-	max_xy := max(x, y)
-	max_xz := max(x, z)
-
-	fmt.Printf("max(%d, %d) = %d\n", x, y, max_xy)
-	fmt.Printf("max(%d, %d) = %d\n", x, z, max_xz)
-	fmt.Printf("max(%d, %d) = %d\n", y, z, max(y, z))
+	fmt.Printf("%d + %d = %d\n", x, y, xPLUSy)
+	fmt.Printf("%d * %d = %d\n", x, y, xTIMESy)
 }
