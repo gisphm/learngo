4e8e9c17f26e8c04576207abb622b8effc3e8bf8
gisphm <phm8341@gmail.com>
2014-05-21 13:37:22 +0000

    override field in inherited struct

--- learngo.go
+++ learngo.go
@@ -1,39 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
-
-type Skills []string
+import "fmt"
 
 type Human struct {
-	name   string
-	age    int
-	weight int
+	name  string
+	age   int
+	phone string
 }
 
-type Student struct {
-	Human      //匿名字段, struct
-	Skills     //匿名字段，自定义类型string slice
-	int        //内置类型作为匿名字段
+type Employee struct {
+	Human
 	speciality string
+	phone      string
 }
 
 func main() {
-	jane := Student{
-		Human:      Human{"Jane", 35, 100},
-		speciality: "Biology"}
-	fmt.Println("Her name is ", jane.name)
-	fmt.Println("Her age is ", jane.age)
-	fmt.Println("Her weight is ", jane.weight)
-	fmt.Println("Her specialityis ", jane.speciality)
-
-	jane.Skills = []string{"anatomy"}
-	fmt.Println("Her skills are ", jane.Skills)
-	fmt.Println("She acquired two new ones ")
-	jane.Skills = append(jane.Skills, "physics", "golang")
-	fmt.Println("Her skills now are ", jane.Skills)
-
-	jane.int = 3
-	fmt.Println("Her preferred number is ", jane.int)
+	Bob := Employee{
+		Human{"Bob", 34, "777-444-xxxx"},
+		"Designer",
+		"333-222"}
+	fmt.Println("Bob's work phone is: ", Bob.phone)
+	fmt.Println("Bob's personal phone is: ", Bob.Human.phone)
 }
