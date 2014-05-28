37e308e90d41dd863ce28b89c0a60cc17e8872c7
gisphm <phm8341@gmail.com>
2014-05-28 12:17:11 +0800

    interface function arguments

--- learngo.go
+++ learngo.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Human struct {
 	name  string
@@ -8,58 +11,11 @@ type Human struct {
 	phone string
 }
 
-type Student struct {
-	Human
-	school string
-	loan   float32
-}
-
-type Employee struct {
-	Human
-	company string
-	money   float32
-}
-
-func (h Human) SayHi() {
-	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
-}
-
-func (h Human) Sing(lyrics string) {
-	fmt.Println("La la la la ...", lyrics)
-}
-
-func (e Employee) SayHi() {
-	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name, e.company, e.phone)
-}
-
-type Men interface {
-	SayHi()
-	Sing(lyrics string)
+func (h Human) String() string {
+	return "<" + h.name + " - " + strconv.Itoa(h.age) + " years - ✆  " + h.phone + ">"
 }
 
 func main() {
-	mike := Student{Human{"Mike", 25, "222-222-XXX"}, "MIT", 0.00}
-	paul := Student{Human{"Paul", 26, "111-222-XXX"}, "Harvard", 100}
-	sam := Employee{Human{"Sam", 36, "444-222-XXX"}, "Golang Inc.", 1000}
-	tom := Employee{Human{"Tom", 37, "222-444-XXX"}, "Things ltd.", 5000}
-
-	var i Men
-
-	i = mike
-	fmt.Println("This is Mike, a Student:")
-	i.SayHi()
-	i.Sing("November rain")
-
-	i = tom
-	fmt.Println("This is Tom, an Employee:")
-	i.SayHi()
-	i.Sing("Born to be world")
-
-	fmt.Println("Let's use a slice of Men and see what happens")
-	x := make([]Men, 3)
-	x[0], x[1], x[2] = paul, sam, mike
-
-	for _, value := range x {
-		value.SayHi()
-	}
+	Bob := Human{"Bob", 39, "000-777-XXX"}
+	fmt.Println("This Human is: ", Bob)
 }
